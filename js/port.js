6a3d02adf3f4f430d6e7af5daffeed3134f88aaf
Not-Aryan <45083845+Not-Aryan@users.noreply.github.com>
2020-08-03 16:47:34 -0700

    Add files via upload

--- js/port.js
+++ js/port.js
@@ -40,3 +40,51 @@ TweenMax.staggerFrom(".media ul li", 1, {
     ease: Expo.easeInOut
 }, 0.2);
 
+
+$(window).scroll(function(){
+  var scroll = $(this).scrollTop();
+  console.log(scroll);
+  scroll < 1170 ? $('#1')[0].play() : $('#1')[0].pause()
+  scroll < 2170 && scroll > 1170 ? $('#2')[0].play() : $('#2')[0].pause()
+  scroll < 3050 && scroll > 2170 ? $('#3')[0].play() : $('#3')[0].pause()
+  scroll > 3050 ? $('#4')[0].play() : $('#4')[0].pause()
+});
+
+// $(window).scroll(function(){
+//     var scroll = $(this).scrollTop();
+//     scroll > 2600 ? $('#2')[0].pause() : $('#2')[0].play()
+// });
+
+
+// $(window).scroll(function(){
+//     var scroll = $(this).scrollTop();
+//     scroll > 3900 ? $('#3')[0].pause() : $('#3')[0].play()
+// });
+
+// $(window).scroll(function(){
+//     var scroll = $(this).scrollTop();
+//     scroll > 5200 ? $('#4')[0].pause() : $('#4')[0].play()
+// });
+
+
+// var myvid = $('#2')[0];
+// $(window).scroll(function(){
+//   var scroll = $(this).scrollTop();
+//   scroll > 70 ? myvid.pause() : myvid.play()
+// });
+
+// var myvid = $('#3')[0];
+// $(window).scroll(function(){
+//   var scroll = $(this).scrollTop();
+//   scroll > 70 ? myvid.pause() : myvid.play()
+// });
+
+// var myvid = $('#4')[0];
+// $(window).scroll(function(){
+//   var scroll = $(this).scrollTop();
+//   scroll > 70 ? myvid.pause() : myvid.play()
+// });
+
+
+
+
